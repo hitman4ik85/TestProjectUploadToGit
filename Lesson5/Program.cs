ca9cbada6c5e3b2870bfbbc43f4068399114a4ed
hitman4ik85 <hitman4ik85@gmail.com>
2024-09-02 20:53:49 +0300

    Kinec Uroky

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,12 +1,87 @@
 ﻿
-using System.Threading.Channels;
+/*using System;
+class Car
+{
+    public string model;
+    public string color;
+    public int year;
+    public  int b = 10000;
 
-Console.WriteLine("Hello, World!");
+    public Car (string model, string color, int year)
+    {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+    public void showInfo()
+    {
+        Console.WriteLine($"Model: {model}, color: {color}, year: {year}");
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Car audi = new Car("A4", "red", 2024);
+        audi.showInfo();
+        audi.model = "A7";
+        audi.showInfo();
+*//*        audi.color = "red";
+        audi.model = "A4";
+        audi.year = 2020;
+
+Car opel = new Car();
+        opel.color = "red";
+        opel.year = 2018;
+        opel.model = "Omega";
+        opel.showInfo();*//*
+    }
+}*/
 
 
+using System;
 
-string name = Console.ReadLine();
-Console.WriteLine($"Hi {name}");
+class Person
+{
+    public string name;
+    public string secondName;
+    public int age;
+    public int id;
+    public int balance;
+    public Person(string name, string secondName, int age, int id)
+    {
+        this.name = name;
+        this.secondName = secondName;
+        this.age = age;
+        this.id = id;
+        this.balance = 1000;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Name: {name},Second Name: {secondName},Age: {age},ID: {id},Balance: {balance}");
+    }
+    public void ZnatieKesh(int sum)
+    {
+        if (sum > balance)
+        {
+            Console.WriteLine("Nema koshtiv!");
+        }
+        else
+        {
+            balance -= sum;
+            Console.WriteLine($"Znjalu: {sum}. Zalushok: {balance}");
+        }
+    }
+}
 
-Console.WriteLine();
-Console.WriteLine();
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person user = new Person("Roma", "Litvi", 30, 12345);
+        user.ShowInfo();
+        user.ZnatieKesh(200);
+        user.ZnatieKesh(1000);
+    }
+}
+
